b08e824f6831495f5d03a29902bb414cf1dfd932
Stevenbarnett1 <Stevenbarnett1@live.com>
2021-07-20 09:11:55 -0700

    added data parser

--- practice-for-week-08-parse-request-body-main/server.js
+++ practice-for-week-08-parse-request-body-main/server.js
@@ -1,1 +1,29 @@
-// Your code here
+const http = require("http")
+
+let server = http.createServer((req,res)=> {
+    let body = ""
+    let obj = {}
+    req.on("data",data=> {
+        body+=data
+    })
+
+    req.on("end", ()=> {
+    console.log(body)
+    body = body.split("&")
+    for(kv of body){
+        kv = kv.split("=")
+        kv[0] = decodeURIComponent(kv[0])
+        kv[1] = kv[1].replace("+"," ")
+        decodeURIComponent(kv[1])
+        obj[kv[0]] = kv[1]
+    }
+    console.log(obj)
+})
+})
+
+
+const port = 3000;
+
+server.listen(port,()=> {
+    console.log(`port is up at ${port}`)
+})
